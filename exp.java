846efc9a77baf8ab3330df3ab268269028ceb537
Bavneet Kaur <171233638+annieee7@users.noreply.github.com>
2025-04-14 18:38:51 +0530

    Create exp.java

---
+++ exp.java
@@ -0,0 +1,63 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Student student = context.getBean(Student.class);
+        student.displayInfo();
+    }
+}
+
+public class Course {
+    private String courseName;
+    private int duration; // in months
+
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Course: " + courseName + ", Duration: " + duration + " months";
+    }
+}
+public class Student {
+    private String name;
+    private Course course;
+
+    public Student(String name, Course course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public void displayInfo() {
+        System.out.println("Student Name: " + name);
+        System.out.println(course);
+    }
+}
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Course course() {
+        return new Course("Java Development", 6);
+    }
+
+    @Bean
+    public Student student() {
+        return new Student("Bavneet", course());
+    }
+}
